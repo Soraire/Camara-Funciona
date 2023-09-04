ea9e54cde35af8dcfd63b0176dbde9e74aea282b
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-04 11:42:54 -0300

    Network Problem

--- camara.js
+++ camara.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet ,Text, View, Button, Image} from 'react-native';
 import { Camera } from 'expo-camera';
+import axios from "axios";
+
 
 export default function Camara() {
 
@@ -21,9 +23,37 @@ const checkPermissions = async () => {
  
   const takePicture = async () => {
     if(camera){
-        const data = await camera.takePictureAsync(null)
+        const data = await camera.takePictureAsync({base64:true})
+        //console.log("data",data)
+        //console.log("data uri", data.uri)
+        //console.log("data base64",data.base64)
         setImage(data.uri);
+        const datajson = {"dni":"44333222",
+        "imagen": data.base64}
+        console.log("!wtf******")
+
+        axios.get('https://api.github.com/users/mapbox')
+  .then((response) => {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  });
+  
+        axios.post("http://10.152.2.113/post_json", datajson)
+        .then(function (response) {
+          console.log(response);
+          console.log("***then")
+        })
+        .catch(function (error) {
+          console.log(error);
+          console.log("***error")
+        });
     }
+
+    
+
   }
  if (hasCameraPermission === false) {
     return <Text>No access to camera</Text>;
