5dffb30cfd54a2d2d63d533c747d87d27757f4e0
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-09 11:39:47 -0300

    Falta test

--- camara.js
+++ camara.js
@@ -1,94 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet ,Text, View, Button, Image} from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 import axios from "axios";
 
-
 export default function Camara() {
-
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
-  
+  const [dni, setDni] = useState(""); // State to hold the DNI value
+
   useEffect(() => {
     checkPermissions();
   }, []);
 
-const checkPermissions = async () => {
-    const {status} = await Camera.requestCameraPermissionsAsync();
+  const checkPermissions = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
     setHasCameraPermission(status === 'granted');
   };
- 
+
   const takePicture = async () => {
-    if(camera){
-        const data = await camera.takePictureAsync({base64:true})
-        //console.log("data",data)
-        //console.log("data uri", data.uri)
-        //console.log("data base64",data.base64)
-        setImage(data.uri);
-        const datajson = {"dni":"44333222",
-        "imagen": data.base64}
+    if (camera) {
+      const data = await camera.takePictureAsync({ base64: true });
+      setImage(data.uri);
 
-       /* axios.get('https://api.github.com/users/mapbox')
-  .then((response) => {
-    console.log(response.data);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);
-  });*/
+      const datajson = new FormData();
+      datajson.append("dni", dni); // Append DNI
+      datajson.append("imagen", {
+        uri: data.uri,
+        type: "image/jpeg",
+        name: "photo.jpg",
+      }); // Append image data
 
-        axios.post("http://localhost:/post_json", datajson)
+      axios.post("http://localhost:/post_json", datajson)
         .then(function (response) {
           console.log(response);
-          console.log("***then")
+          console.log("***then");
         })
         .catch(function (error) {
-          console.log("El error es" + error)
+          console.log("El error es" + error);
           console.log(error);
-          console.log("***error")
+          console.log("***error");
         });
     }
+  };
 
-    
-
-  }
- if (hasCameraPermission === false) {
+  if (hasCameraPermission === false) {
     return <Text>No access to camera</Text>;
   }
-  
+
   return (
- 
     <View style={styles.container2}>
- 
+      {/* Input for DNI */}
+      <input
+        type="int"
+        id="DNI"
+        name="DNI"
+        value={dni}
+        onChangeText={(int) => setDni(int)}
+      />
+
       <View style={styles.cameraContainer}>
-        <Camera 
-            ref={ref => setCamera(ref)}
-            style={styles.camera} 
-            type={type}
-            ratio={'1:1'} />
-        </View>
+        <Camera
+          ref={(ref) => setCamera(ref)}
+          style={styles.camera}
+          type={type}
+          ratio={'1:1'}
+        />
+      </View>
 
-      <Button      style={styles.button}
-            title="Flip Image"
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-     </Button>
-     <Button title="Take Picture" onPress={() => takePicture()} />
-     {image && <Image source={{uri: image}} style={{flex:1}}/>}
+      <Button
+        style={styles.button}
+        title="Flip Image"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}
+      />
+      <Button title="Take Picture" onPress={() => takePicture()} />
+      {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
     </View>
-
   );
-
-  
 }
+
 
 const styles = StyleSheet.create({
   container: {
